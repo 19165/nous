11c7944f34bf02c597f8ebc4ef78a616104ee7af
Prudtipon JITWIRIYANON <s.crossxE@gmail.com>
2026-06-09 21:40:49 +0700

    use TavilySearch from langchain-tavily instead TavilyResultSearch (hallucinated object)

--- src/tools/search.py
+++ src/tools/search.py
@@ -1,18 +1,20 @@
 import os
-from langchain_tavily import TavilySearchResults
+from langchain_tavily import TavilySearch
 from dotenv import load_dotenv
+from functools import lru_cache
 
 load_dotenv()
 
+
+@lru_cache(maxsize=1)
 def get_web_search_tool():
     """
     Returns a Tavily web search tool for general research.
     """
-    return TavilySearchResults(
-        max_results=5,
-        search_depth="advanced"
-    )
+    return TavilySearch(max_results=5, topic="general")
 
+
+@lru_cache(maxsize=1)
 def get_news_search_tool():
     """
     Returns a Tavily search tool optimized for news.
@@ -20,8 +22,7 @@ def get_news_search_tool():
     # Note: topic="news" is supported in the underlying Tavily API
     # and passed through via kwargs in some versions, or explicitly in others.
     # In langchain-tavily, we can pass it to the constructor.
-    return TavilySearchResults(
+    return TavilySearch(
         max_results=5,
         topic="news",
-        search_depth="advanced"
     )
